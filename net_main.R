9a3bf6f314916648e47cdb4e03ca28ec3d39baa2
Flora Murakeozy-Kis <fmurakeozykis@gmail.com>
2025-05-20 07:22:19 +0200

    Update net_main.R

--- net_main.R
+++ net_main.R
@@ -1,114 +1,86 @@
 source("C:/Users/fmura/Documents/groningen/New folder/network.R")
 source("C:/Users/fmura/Documents/groningen/New folder/all_parameters.R")
 library(igraph)
-library(openxlsx)
+library(tidyr)
+library(ggplot2)
 
-#potential issues:
-### it produces 1 list of random nodes, which is reused with every run
-### change length to sample size
 
-##remove same trait part
 
-cols <- c("node", "trait", "connections")
+cols <- c("node", "trait")
 properties_1 <- data.frame(
-  node = V(g),
-  trait = rep("resident", length(g)),
-  connections = deg_seq_1
+  node = (V(g)),
+  trait = rep("resident", n)
 )
-print(migrate)
-vary_migration <- function (migrate, random, properties_1, g, length){
-  for(x in migrate){ 
-  #randomly select an individual from the network, place in vector
-  random <- sample(1:length(g), size = length, replace = F)
 
-   interaction_1 <- function (random, properties_1, g, x) {
-  for (i in random) {
-    migration <- rbinom(1, size = 1, prob = x)
-      if (migration == 1){ 
+properties_1 <- data.frame(
+  node = as.integer(V(g)), 
+  trait = rep("resident", n)
+)
+
+vary_migration <- function(migrate, num_events, properties_1, g) {
+  for (x in migrate) {
+    cat("\n--- Starting migration rate:", x, "---\n")
+    
+    for (event in 1:num_events) {
+      
+      i <- sample(as.integer(V(g)), 1)
+      
+      migrate_now <- rbinom(1, 1, x)
+      if (migrate_now == 1) {
         properties_1$trait[properties_1$node == i] <- "immigrant"
-        cat(i, "replaced by immigrant\n")
-      }
-    else {
-      connected_nodes <- neighbors(g, i)
-      s <- sample(connected_nodes, 1, F)
-      cat("current node:", i, "\n")
-      cat("connections:", connected_nodes, "\n")
-      cat("chosen:", s, "\n")
-      if (properties_1$trait[properties_1$node == i] == "resident"){ 
-        if (properties_1$trait[properties_1$node == s] == "resident"){
-          interact <- rbinom(1, size = 1, prob = rwithr)
-          cat("both residents\n")
+      } else {
+        partners <- as.integer(V(g))[as.integer(V(g)) != i]  
+        s <- sample(partners, 1)
+        
+        trait_i <- properties_1$trait[properties_1$node == i]
+        trait_s <- properties_1$trait[properties_1$node == s]
+        
+        if (trait_i == "resident" && trait_s == "resident") {
+          interact <- rbinom(1, 1, rwithr)
           if (interact == 1) {
-            take_over <- rbinom(1, size = 1, prob = c_r)
-            cat("they were social\n")
-            if (take_over == 1){
-              properties_1$trait[properties_1$node == i] <- properties_1$trait[properties_1$node == s]
-              cat("focal took over partner's culture\n")
+            take_over <- rbinom(1, 1, 1 - c_r)
+            if (take_over == 1) {
+              properties_1$trait[properties_1$node == i] <- trait_s
             }
-            else {cat("focal conserved their culture\n")}
           }
-          else {cat("they were antisocial\n")}
-        }
-        else {
-            interact <- rbinom(1, size = 1, prob = rwithi)
-            cat("focal = resident, partner = immigrant\n")
-            if (interact == 1) {
-              take_over <- rbinom(1, size = 1, prob = c_r)
-              cat("they were social\n")
-              if (take_over == 1){
-                properties_1$trait[properties_1$node == i] <- properties_1$trait[properties_1$node == s]
-                cat("focal took over partner's culture\n")
-              }
-              else {cat("focal conserved their culture\n")}
+        } else if (trait_i == "resident" && trait_s == "immigrant") {
+          interact <- rbinom(1, 1, rwithi)
+          if (interact == 1) {
+            take_over <- rbinom(1, 1, 1 - c_r)
+            if (take_over == 1) {
+              properties_1$trait[properties_1$node == i] <- trait_s
             }
-            else {cat("they were antisocial\n")}
-        }
-      }
-      else {
-          if (properties_1$trait[properties_1$node == s] == "resident"){
-            interact <- rbinom(1, size = 1, prob = iwithr)
-            cat("both residents\n")
-            if (interact == 1) {
-              take_over <- rbinom(1, size = 1, prob = c_i)
-              cat("they were social\n")
-              if (take_over == 1){
-                properties_1$trait[properties_1$node == i] <- properties_1$trait[properties_1$node == s]
-                cat("focal took over partner's culture\n")
-              }
-              else {cat("focal conserved their culture\n")}
-            }
-            else {cat("they were antisocial\n")}
           }
-          else {
-            interact <- rbinom(1, size = 1, prob = iwithi)
-            cat("focal = immigrant, partner = immigrant\n")
-            if (interact == 1) {
-              take_over <- rbinom(1, size = 1, prob = c_i)
-              cat("they were social\n")
-              if (take_over == 1){
-                properties_1$trait[properties_1$node == i] <- properties_1$trait[properties_1$node == s]
-                cat("focal took over partner's culture\n")
-              } else {cat("focal conserved their culture\n")}
-            } else {cat("they were antisocial\n")}
+        } else if (trait_i == "immigrant" && trait_s == "resident") {
+          interact <- rbinom(1, 1, rwithi)
+          if (interact == 1) {
+            take_over <- rbinom(1, 1, 1 - c_i)
+            if (take_over == 1) {
+              properties_1$trait[properties_1$node == i] <- trait_s
+            }
+          }
+        } else if (trait_i == "immigrant" && trait_s == "immigrant") {
+          interact <- rbinom(1, 1, iwithi)
+          if (interact == 1) {
+            take_over <- rbinom(1, 1, 1 - c_i)
+            if (take_over == 1) {
+              properties_1$trait[properties_1$node == i] <- trait_s
+            }
           }
         }
       }
-     } 
-     return(properties_1)
     }
     
-    cat("\n", "with migration rate", x, "\n")
-    properties_1 <- interaction_1(random, properties_1, g, x)
+    migr <- paste0("mig_", x)
+    properties_1[[migr]] <- properties_1$trait
+    cat("Saved traits for migration rate:", x, "\n")
     
+    properties_1$trait <- rep("resident", n)
   }
+  
   return(properties_1)
 }
 
-properties_1 <- vary_migration(migrate, NULL, properties_1, g, samplesize)
-
-
-#write.csv(properties_1, "C:\Users\fmura\Documents\groningen\New folder\test_output.xlsx")
-#write.xlsx(properties_1, "C:/Users/fmura/Documents/groningen/New folder/test.xlsx", sheetName = "Sheet1", overwrite = TRUE)
-#write.xlsx(graph, "C:/Users/fmura/Documents/groningen/New folder/plot.xlsx", sheetName = "Sheet2", overwrite = TRUE)
-
-
+system.time({
+  properties_1 <- vary_migration(migrate, num_events, properties_1, g)
+})
