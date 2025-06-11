04a891d6bd7f585eb86cf4660ea81b73b637f3c6
Flora Murakeozy-Kis <fmurakeozykis@gmail.com>
2025-06-11 22:48:29 +0200

    Update plotting.R

--- plotting.R
+++ plotting.R
@@ -69,4 +69,57 @@ plot_hom <- function(data, interaction_prob) {
   return(p_hom)
 }
 
+#########################################
+####### AVERAGE #########################
+#########################################
+
+plot_overlay <- function(list_of_datasets, plot_title = NULL, plot_subtitle = NULL) {
+  combined_data <- dplyr::bind_rows(
+    lapply(list_of_datasets, readRDS)
+  )
+  
+  if (is.null(plot_title)) {
+    plot_title <- "Resident fraction overlay plot"
+  }
+  
+  ggplot(combined_data, aes(x = mig_rate, y = c_i, z = resident_fraction)) +
+    geom_tile(aes(fill = resident_fraction), alpha = 0.6) +
+    
+    geom_contour(
+      aes(linetype = as.factor(after_stat(level))),
+      breaks = c(1, 0.5, 0.25, 0.1),
+      color = "black",
+      size = 0.8
+    ) +
+    
+    scale_linetype_manual(
+      name = "Resident frequency (contour)",
+      values = c("1" = "solid", "0.5" = "dashed", "0.25" = "dotdash", "0.1" = "dotted")
+    ) +
+    
+    scale_fill_gradient2(
+      low = "red", mid = "white", high = "blue", midpoint = 0.5,
+      name = "Resident frequency"
+    ) +
+    
+    labs(
+      x = "Migration rate",
+      y = "Immigrant conservatism (c_i)",
+      title = plot_title,
+      subtitle = plot_subtitle
+    ) +
+    
+    theme_classic()
+}
+
+plot_overlay(
+  list_of_datasets = c(
+    "data/fin_hom05_run1.RDS", "data/fin_hom05_run2.RDS", "data/fin_hom05_run3.RDS",
+    "data/fin_hom05_run4.RDS", "data/fin_hom05_run5.RDS", "data/fin_hom05_run6.RDS",
+    "data/fin_hom05_run7.RDS", "data/fin_hom05_run8.RDS"
+  ),
+  plot_title = "Resident fraction for homogenous population\nInteraction probability = 0.5",
+  plot_subtitle = "Average over 8 runs"
+)
+
 
