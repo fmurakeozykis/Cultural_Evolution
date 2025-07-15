9e4def9424bfa03d337191b87daeb1c13f9ac458
Flora Murakeozy-Kis <fmurakeozykis@gmail.com>
2025-07-15 13:13:47 +0200

    Update plotting.R

--- plotting.R
+++ plotting.R
@@ -1,13 +1,20 @@
+################################################################################
+# Simulation: Social Net Acculturation Model - Plot Functions
+# Author: Flora Murakeozy-Kis
+# Date: July 2025
+#
+# Description:
+# Plot functions for heatmaps of resident cultural trait retention 
+# 10x10 immigrant cultural conservatism values and migration rates. 
+###################################################################
+# SOURCES 
+
 library(ggplot2)
 
-################################################################################
-###### Functions for plotting the results ######################################
-################################################################################
+###################################################################
+# PLOTTING
 
-#plots are also now in functions, so that it can just take the input from parameters 
-## and you dont have to reweite everything yourself
-# i have to add the plots here, all of them
-# they will also go to a spearatte folder?
+# WELL-MIXED POPULATION  -----------------------------------------
 
 plot_wellmixed <- function(data, interaction_prob) {
   title <- paste0("Interaction Probability = ", interaction_prob)
@@ -27,9 +34,7 @@ plot_wellmixed <- function(data, interaction_prob) {
   return(p_wellmixed)
 }
 
-##############################
-###NETWORK####################
-#############################
+# HETEROGENEOUS NETWORK POPULATION  -----------------------------------------
 plot_network <- function(data, interaction_prob) {
   title <- paste0("Interaction Probability = ", interaction_prob)
   p_network<- ggplot(data, aes(x = mig_rate, y = c_i, fill = resident_fraction)) +
@@ -48,9 +53,7 @@ plot_network <- function(data, interaction_prob) {
   return(p_network)
 }
 
-##############################
-###HOMOGENOUSK####################
-#############################
+# HOMOGENEOUS NETWORK POPULATION  -----------------------------------------
 plot_hom <- function(data, interaction_prob) {
   title <- paste0("Interaction Probability = ", interaction_prob)
   p_hom<- ggplot(data, aes(x = mig_rate, y = c_i, fill = resident_fraction)) +
@@ -69,57 +72,5 @@ plot_hom <- function(data, interaction_prob) {
   return(p_hom)
 }
 
-#########################################
-####### AVERAGE #########################
-#########################################
-
-plot_overlay <- function(list_of_datasets, plot_title = NULL, plot_subtitle = NULL) {
-  combined_data <- dplyr::bind_rows(
-    lapply(list_of_datasets, readRDS)
-  )
-  
-  if (is.null(plot_title)) {
-    plot_title <- "Resident fraction overlay plot"
-  }
-  
-  ggplot(combined_data, aes(x = mig_rate, y = c_i, z = resident_fraction)) +
-    geom_tile(aes(fill = resident_fraction), alpha = 0.6) +
-    
-    geom_contour(
-      aes(linetype = as.factor(after_stat(level))),
-      breaks = c(1, 0.5, 0.25, 0.1),
-      color = "black",
-      size = 0.8
-    ) +
-    
-    scale_linetype_manual(
-      name = "Resident frequency (contour)",
-      values = c("1" = "solid", "0.5" = "dashed", "0.25" = "dotdash", "0.1" = "dotted")
-    ) +
-    
-    scale_fill_gradient2(
-      low = "red", mid = "white", high = "blue", midpoint = 0.5,
-      name = "Resident frequency"
-    ) +
-    
-    labs(
-      x = "Migration rate",
-      y = "Immigrant conservatism (c_i)",
-      title = plot_title,
-      subtitle = plot_subtitle
-    ) +
-    
-    theme_classic()
-}
-
-plot_overlay(
-  list_of_datasets = c(
-    "data/fin_hom05_run1.RDS", "data/fin_hom05_run2.RDS", "data/fin_hom05_run3.RDS",
-    "data/fin_hom05_run4.RDS", "data/fin_hom05_run5.RDS", "data/fin_hom05_run6.RDS",
-    "data/fin_hom05_run7.RDS", "data/fin_hom05_run8.RDS"
-  ),
-  plot_title = "Resident fraction for homogenous population\nInteraction probability = 0.5",
-  plot_subtitle = "Average over 8 runs"
-)
 
 
