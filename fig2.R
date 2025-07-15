7cf7059e6ff56f1770da5a7523f7cf5d7c6e481e
Flora Murakeozy-Kis <fmurakeozykis@gmail.com>
2025-07-15 12:48:30 +0200

    Update fig2.R

--- fig2.R
+++ fig2.R
@@ -21,7 +21,7 @@ resultsn <- readRDS("data/network05.RDS")
 plot05net <- plot_network(resultsn,0.5)
 ggsave("figures/net_0.5.pdf", plot = plot05net, width = 8, height = 6)
 
-# INTERACTION PROBABILITY = 0.1 -----------------------------------
+# INTERACTION PROBABILITY = 0.5 -----------------------------------
 resultsn <- readRDS("data/network01.RDS")
 plot01net <- plot_network(resultsn,0.1)
 ggsave("figures/net_0.1.pdf", plot = plot01net, width = 8, height = 6)
